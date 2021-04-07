c2c4e00cc5c043b1deb9abb186980e486ad383f0
Mukesh Murugan <iammukeshm@gmail.com>
2021-04-07 19:59:17 +0530

    codemaid-cleanup

--- src/Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ src/Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WebApi.Boilerplate.Infrastructure.Extensions
+﻿namespace WebApi.Boilerplate.Infrastructure.Extensions
 {
-    class ServiceCollectionExtensions
+    internal class ServiceCollectionExtensions
     {
     }
 }
