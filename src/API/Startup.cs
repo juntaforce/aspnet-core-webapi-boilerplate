1ad16be5b7ba369a01be52bef0afdb12aec7c70e
Mukesh Murugan <iammukeshm@gmail.com>
2021-04-07 20:21:29 +0530

    swagger addition

--- src/API/Startup.cs
+++ src/API/Startup.cs
@@ -22,7 +22,7 @@ namespace WebApi.Boilerplate.API
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApi.Boilerplate.API", Version = "v1" });
             });
         }
 
@@ -33,7 +33,7 @@ namespace WebApi.Boilerplate.API
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi.Boilerplate.API v1"));
             }
 
             app.UseHttpsRedirection();
