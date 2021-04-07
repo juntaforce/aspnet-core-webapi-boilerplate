146b7c22902c745b0a2e49976951346dfba1f17e
Mukesh Murugan <iammukeshm@gmail.com>
2021-04-08 01:21:08 +0530

    added serilog

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using WebApi.Boilerplate.API.Extensions;
 
 namespace WebApi.Boilerplate.API
 {
@@ -12,6 +13,7 @@ namespace WebApi.Boilerplate.API
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+             .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
