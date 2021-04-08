2a5e2ab91a8bbd4abaa5eb3dcc7ed20146e92d3a
Mukesh Murugan <iammukeshm@gmail.com>
2021-04-08 23:30:47 +0530

    added product configuration - sample

---
+++ src/Infrastructure/Configurations/ProductConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WebApi.Boilerplate.Domain.Entities;
+
+namespace WebApi.Boilerplate.Infrastructure.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(p => p.Id).IsRequired();
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(50);
+            builder.Property(p => p.Price).HasColumnType("decimal(18,2)");
+        }
+    }
+}
