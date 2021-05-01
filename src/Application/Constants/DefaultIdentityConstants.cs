2c9f5edbbaebdb8d13d862e7bd2e85b891773310
Mukesh Murugan <iammukeshm@gmail.com>
2021-05-01 18:09:58 +0530

    updated constants

--- src/Application/Constants/DefaultIdentityConstants.cs
+++ src/Application/Constants/DefaultIdentityConstants.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WebApi.Boilerplate.Application.Constants
+﻿namespace WebApi.Boilerplate.Application.Constants
 {
     public static class DefaultIdentityConstants
     {
         public static class DefaultAdministrator
         {
             public const string UserName = "administrator";
-            public const string Email = "admin@gmail.com";
+            public const string Email = "admin@fullstackhero.com";
             public const string FirstName = "Mukesh";
             public const string LastName = "Murugan";
             public const string Password = "123Pa$$word!";
@@ -19,7 +13,7 @@ namespace WebApi.Boilerplate.Application.Constants
         public static class DefaultBasicUser
         {
             public const string UserName = "basicuser";
-            public const string Email = "basicuser@gmail.com";
+            public const string Email = "basicuser@fullstackhero.com";
             public const string FirstName = "John";
             public const string LastName = "Doe";
             public const string Password = "123Pa$$word!";
